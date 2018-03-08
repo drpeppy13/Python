97cea034323059e903dfe6728e85d563ff5b16c8
Kyle <kmkimis6@gmail.com>
2018-03-08 10:55:24 -0800

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+print " Hello World! "
+
+myDictionary = {
+    "name":"Gerso",
+    "position":"student",
+    "hobby":"coding"
+}
+print myDictionary["position"]
+myDictionary["position"] = "Coding Master"
+print myDictionary
+
