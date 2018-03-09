72d1cb9ef11dfb89e86b290519230824b8c5bc5f
Kyle <kmkimis6@gmail.com>
2018-03-08 23:50:59 -0800

    python assignments

---
+++ stringnlist.py
@@ -0,0 +1,16 @@
+# words = "It's thanksgiving day. It's my birthday, too!"
+# print words.find("day")
+# print words.replace("day", "month")
+
+# myList = [2,54,-2,7,12,98]
+# print max(myList)
+# print min(myList)
+
+# x = ["hello",2,54,-2,0.5,7,12,98,"world"]
+# print x[0::len(x)-1]
+# print x[0], x[-1]
+
+x = [19,2,54,-2,7,12,98,32,10,-3,6]
+x.sort() 
+print x[[0]:[len(x)/2]]
+print x[len(x)/2:len(x)-1]
