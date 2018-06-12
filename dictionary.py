efd41a00e2fdb016cdace3bda15b658df48ff41d
Kyle Kim <kmkimis6@gmail.com>
2018-06-11 22:20:35 -0700

    revision

--- dictionary.py
+++
@@ -1,6 +0,0 @@
-name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
-favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
-# dictionary = dict(zip(name, favorite_animal))
-# print(dictionary)
-from itertools import izip
-print dict(izip(name,favorite_animal))
--- intro.py
+++
@@ -1,9 +0,0 @@
-my_list = [41, 23]
-my_second_list = [
-    42,24
-]
-print my_list + my_second_list
-print my_list, my_second_list
-print (my_list, my_second_list)
-print [my_list, my_second_list]
-print [my_list, my_second_list]
